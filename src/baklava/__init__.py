0a0a5ef44ef5bd3e49d77f4d5e1025cde26930e4
danny-lloyd <danny.lloyd@stfc.ac.uk>
2024-09-03 14:02:13 +0100

    Initial commit

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Danny Lloyd"
+__email__ = "danny.lloyd@stfc.ac.uk"
+__version__ = "0.1.0"
